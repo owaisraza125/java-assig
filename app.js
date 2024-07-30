75e3f28038c26e10977012f580c4f3806d9568f8
Owais Raza <125razaowais@gmail.com>
2024-07-30 21:54:49 +0500

    Web design complete

---
+++ app.js
@@ -0,0 +1,492 @@
+
+                            //    Chapter 1 (Alerts)
+
+// Qno=1,Alert these following (individually):                             
+var a = prompt("First Name");
+alert (a);
+
+var b = prompt("Last Name");
+alert (b);
+
+var c = prompt("Email");
+alert (c);
+
+var d = prompt("Phone Number");
+alert (d);
+
+var e = prompt("Password");
+alert (e);
+
+// Qno=2,Correct this statement: alert"You're learning JavaScript!";
+alert ("You're learning JavaScript!");
+
+
+// Qno3. Code an alert statement displaying any message you like.
+alert ("We are here to teach you javascript ");
+
+
+
+                            //   Chapter 2 (Variables for string)
+// Qno1. Declare any variable in the camelCase format.
+var studentAge= "25";
+
+// Qno2. Declare a variable of your choice without defining it. Then, in a
+// second statement, assign it a string of your choice.
+
+var Message;
+Message="learning js";
+
+// Qno3. Declare the variable teamName and Alert your Team name.
+var a ="teamName=";
+var b ="JawanPakistan";
+var c = a+b;
+alert(c)
+
+                            // Chapter 3 (Variables for numbers)
+//   Qno1. Declare a variable “caseQty”      
+
+
+
+
+
+// Qno5. What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// Try it yourself.
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert (orderTotal)
+
+
+// Qno6. In the first statement declare a variable and assign it a number. In
+// the second statement, change the value of the variable by adding
+// it together with a number.
+var a =10;
+var b =20;
+var c = a+b
+alert(c)
+
+                            // Chapter 4 (Variable names Legal andIllegal)
+// Qno1. Correct this statement.var product cost = 3.45;
+var productCost = 3.45;
+
+// Qno2. Rewrite this using camelCase.
+// var Nameofband;
+var nameOfBand;
+
+// Qno2. In a single statement declare a legally-named variable and assign a
+// number to it.
+ var totalStudents = 72;
+ 
+//  Qno3. Declare a variable that is a combination of your first and last
+//  names. Use camelCase.
+var owaisRaza;
+
+// Qno5. List the legal and Illegal Variables.
+
+// Legal Variables:
+ var owaisRaza;
+ var total_amount;
+
+//  Illegal Variables:
+
+// var @value 2
+// var 2ndValue;
+
+
+
+                                        // Chapter 5 (Math Expression I)
+// Qno1. What is the name and symbol of the arithmetic operator that
+// gives you the remainder when one number is divided by another?
+
+// Name: Modulus operator
+// Symbol: %
+
+// Qno2. What is the value of num?
+// var num = 20 % 6;
+
+var value =2
+
+// Qno3. In a single statement, declare the variable largeNum and assign it
+// the result of 1,000 multiplied by 2,000.
+var a =1000 * 2000;
+alert (a)
+
+// Qno4. Assign to a variable the value represented by one variable
+// subtracted from the value represented by another variable
+var a =2;
+var b =3;
+var c =2-3;
+alert (c)
+
+// Qno5. Code an alert that displays the result of a multiplication on 2
+// numbers.
+var a =2
+var b =2
+var c =a*b
+alert (c)
+
+
+
+                                    // Chapter 6 (Math Expression II)
+
+// Qno1. Code a short form of x = x + 1; Use either of the two legal
+// expressions.
+// first post-increment
+var x =2
+var b =x++
+alert (b)
+
+ //pre-increment
+ var x =2
+var b =++x
+alert (b)
+
+// Qno2. If x has a value of 100, what is the fastest way to reduce it to 99
+// with a math expression?
+var x =100
+var c =x-1
+alert (c)
+
+// 3. var x = 50;
+// var y = x++;
+// What is the value of y?
+var x = 50;
+var y = x++
+ alert (y)   //(50)
+
+//  4. var y = 50;
+// var z = --y;
+// What is the value of z?
+var y = 50;
+var z = --y
+alert (z)  //z=49
+
+// Qno5. In a single statement, decrement num and assign its original value
+// to newNum.
+newNum = num-- ;
+
+// Qno6. In a single statement add 1 to a variable and assign its original
+// value to another variable.
+newVar = oldVar++;
+
+// 7. Assign a number value to a variable.
+// Increment the variable.
+// Display the new value in an alert
+var a =3
+var b =++a
+alert (b)
+
+                            //   Chapter 7 (Math Expression III)
+
+// 1. var calculatedNum = 2 + (2 * 6);
+// What is the value of calculatedNum?
+var calculatedNum = 2 + (2 * 6);
+var theCalculatedNum =  14;
+
+// Qno3. var calculatedNum = (2 + 2) * (4 + 2);
+// What is the value of calculatedNum?
+var calculatedNum = (2 + 2) * (4 + 2);
+var valueOfCalculatedNum = 18
+
+// Qno4. var calculatedNum = ((2 + 2) * 4) + 2;
+// What is the value of calculatedNum?
+var calculatedNum = ((2 + 2) * 4) + 2;
+var valueOfCalculatedNum =  18
+
+// Qno5. Write a statement that assigns to cost the result of 2 + 2 * 4 + 10,
+// clarified with parentheses, producing 56.
+var costTheResult =(2+2) * (4+10)
+var answeres =56  //hence proved
+
+// 6. Write a statement that assigns to units the result of 2 + 2 * 4 + 10,
+// clarified with parentheses, producing 20.
+unitsTheResult = (2 + 2 * 4) + 10;
+answeres = 20 //hence proved
+
+
+                    //    Chapter 8 (Concatenating TextStrings)
+
+// Qno1. var num = "2" + "2";
+// What is the value of num? Include quotation marks.
+var num = "2" + "2";
+number =22
+
+
+// Qno2. message = ("Hello," + "Dolly");
+// What is the value of message? (Include the quotation marks.)
+// Alert the statement
+message = ("Hello," + "Dolly")
+alert(message);
+
+// Qno3. alert("33" + 3);
+// What message displays in the alert box?
+alert("33" + 3);   //333
+
+// Qno4. Write an alert that displays the concatenation of the two parts of
+// "Pakistan Zindabad".
+alert("Pakistan" + " Zindabad");
+
+// 5. Write a statement that assigns to a variable the concatenation of
+// a string with a number
+var a = "My age is " + 16;
+
+// Qno6. Assign strings to two variables. Then concatenate them and assign
+// the result to a third variable.
+var a ="hello"
+var b ="world"
+var c =a+b
+alert(c)
+
+                                    //   Chapter 9 (Prompts)
+
+// 1. Code a prompt with the message "Enter first name". The user's
+// response is assigned to firstName.
+var a = prompt("First Name");
+alert (a);
+
+// 2. Code a prompt with the message "Country?" and the default
+// answer "China". The user's response is assigned to country.
+alert= prompt("Country?", "China");
+
+// 3. Correct this statement
+// var yourName = prompt(Enter Your Name");
+var yourName = prompt("Enter Your Name");
+
+// 4. Code a prompt that specifies a string as the message Include a
+// default input.
+let userInput = prompt("Please enter your favorite color:", "Blue");
+
+console.log(userInput);
+
+                                        // Chapter 10 (if statments)
+
+// 1. var city = "Karachi"
+// if (city = "Karachi") {
+// console.log("The City OF Lights")
+// Correct the above statement:
+// Also try this statement by yourself
+var city = "Karachi";
+if (city === "Karachi") {
+    console.log("The City OF Lights");
+}
+
+// 2. This is the first line of an if statement:
+// if (x === y) {
+// Complete the statement. If the condition is true, display a box
+// that asks the user value of z? and assign it to another variable.
+if (x === y) {
+    var zValue = prompt("Please enter the value of z:");
+}
+
+// 3. Code an if statement that tests if ZipCode is "10010" so, Alert
+// that "Karachi". if not then alert ("Please write correct city")
+var ZipCode = "10010"; // Example value, you can set this as needed
+
+if (ZipCode === "10010") {
+    alert("Karachi");
+} else {
+    alert("Please write correct city");
+}
+
+// 4. Code an if statement. Test whether a variable has a particular
+// numerical value. If so, assign a new value to that variable, as in
+// x = 1;
+var x = 1; // Example initial value
+
+if (x === 1) {
+    x = 5; 
+}
+
+console.log(x); 
+  
+                                        //   Chapter 11 (ComparisonOperators)
+// 1. Code the first line of an if statement that tests whether one
+// variable is unequal to another. (Use !)
+if (x != y) {}
+
+// 2. Code the first line of an if statement that tests whether the
+// value represented by a variable is greater than or equal to the
+// value represented by another variable.
+ if (x >= y) {}
+
+
+// 3. Code an if statement. Test whether a variable is unequal to a
+// particular number. If so, assign a number to that variable.
+var x = 5; 
+
+if (x !== 10) {
+    x = 20; 
+}
+
+console.log(x);
+
+// 4. Code an if statement that tests whether a number is unequal to
+// a different number. If the condition is true (it will be), display a
+// congratulations alert.
+var a = 5; 
+var b = 10; 
+
+if (a !== b) {
+    alert("Congratulations!");
+}
+
+
+// 5. Code a prompt asking for your first name.
+// Code an if statement that tests whether the name you entered
+// is unequal to another name.
+// If the condition is true (it will be), display an alert that says "No
+// match"
+var firstName = prompt(" your first name:");
+
+if (firstName !== "John") { 
+    alert("No match");
+}
+
+
+                    //    Chapter 12 (if...else and else ifstatements)
+// 1. Code an if statement that tests whether the value represented by
+// a variable is greater than or equal to the value represented by
+// another variable. If so, display an alert. If not, display a different
+// alert.
+// Example variables
+let a = 5; 
+let b = 3; 
+
+if (a >= b) {
+    alert("The value of 'a' is greater than or equal to 'b'");
+} else {
+    alert("The value of 'a' is less than 'b'");
+}
+
+
+// 2. Write a program using if else and else if statement which take
+// marks from user. And the program will calculate your percentage
+// and give you grade A/C to Your percentage. (MARKSHEET)
+// Prompt the user to enter their marks
+let marks = parseFloat(prompt("Enter your marks:"));
+
+
+let percentage = marks; 
+if (percentage >= 90) {
+    alert("Grade: A");
+} else if (percentage >= 80) {
+    alert("Grade: B");
+} else if (percentage >= 70) {
+    alert("Grade: C");
+} else if (percentage >= 60) {
+    alert("Grade: D");
+} else {
+    alert("Grade: F");
+}
+
+
+// 3. This is the if statement that begins the code.
+// if (a === 10) {
+// alert("a is 10");
+// }
+// If a isn't 10, display an alert that says The correct value of a is
+// ____
+// Note: Try this by yourself
+let a = 7; 
+
+if (a === 10) {
+    alert("a is 10");
+} else {
+    alert("The correct value of a is " + a);
+}
+
+// 4. Prompt the user to enter a city.
+// If the city is Karachi, display an alert acknowledging it is Karachi.
+// If not, check to see if it's Lahore.
+// If it is, display an alert acknowledging it's Lahore.
+// Otherwise, display a different alert.
+// Prompt the user to enter a city
+let city = prompt("Enter a city:");
+
+if (city === "Karachi") {
+    alert("This is Karachi");
+} else if (city === "Lahore") {
+    alert("This is Lahore");
+} else {
+    alert("This city is neither Karachi nor Lahore");
+}
+
+
+                        //  Chapter 13 (Testing sets ofconditions)
+
+
+// 1. Code the first line of an if statement that tests whether both are
+// true: a has the same value as b and c has the same value as d.
+// Example variables
+let a = 5;
+let b = 5;
+let c = 10;
+let d = 10;
+
+
+if (a === b && c === d) {
+    
+}
+
+
+// 2. Code the first line of an if statement that tests whether either or
+// both are true: a has the same value as b or c doesn't have the
+// same value as d.
+// Example variables
+let a = 5;
+let b = 5;
+let c = 10;
+let d = 15;
+
+
+if (a === b || c !== d) {
+    
+}
+
+// 3. Code the first line of an if statement that tests whether
+// I. name is either "Hamza" or "Arsalan".
+// II. age is Less than 60.
+// Example variables
+let name = "Hamza";
+let age = 25;
+
+
+if ((name === "Hamza" || name === "Arsalan") && age < 60) {
+    
+}
+
+
+// 4. Declare two variables and assign them number values.
+// If the first variable is less than the second variable or greater than
+// the second variable, display an alert.
+// Declare and assign values
+let num1 = 5;
+let num2 = 10;
+
+
+if (num1 < num2 || num1 > num2) {
+    alert("The first variable is either less than or greater than the second variable");
+}
+
+
+// 5. Declare 2 variables. Assign one of them your first name and the
+// other one your last name.
+// Code 2 prompts, asking for your first and your last name.
+// If your answers match the two variables, display an alert.
+// Declare and assign values
+let myFirstName = "John";
+let myLastName = "Doe";
+
+
+let inputFirstName = prompt("Enter your first name:");
+let inputLastName = prompt("Enter your last name:");
+
+
+if (inputFirstName === myFirstName && inputLastName === myLastName) {
+    alert("Your name matches!");
+}
+
